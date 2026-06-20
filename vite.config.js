91da15bf971ed442cf0858349c93681da4108ade
L-Tiger <kimbaejang2025@gmail.com>
2026-06-20 11:16:22 +0700

    cấu hình base path cho github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/luyen-thi-tin-hoc-12/', // Thêm chính xác dòng này (tên repository của bạn)
 })
