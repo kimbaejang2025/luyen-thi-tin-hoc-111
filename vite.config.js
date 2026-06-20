3f235b868cabee6a4de93222415d11b6540d9a47
L-Tiger <kimbaejang2025@gmail.com>
2026-06-20 19:15:50 +0700

    cấu hình base path cho repo moi luyen-thi-tin-hoc-111

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/luyen-thi-tin-hoc-12/', // Thêm chính xác dòng này (tên repository của bạn)
+  base: '/luyen-thi-tin-hoc-111/', // Thêm chính xác dòng này (tên repository của bạn)
 })
